a1d6eadfdd1f8c41dbec7e6a43564bf800a0a13c
Jhonatahan Rivera <jonathan02rv@gmail.com>
2024-11-05 12:42:59 -0500

    add travis file

---
+++ CountriesData/CountriesData.h
@@ -0,0 +1,18 @@
+//
+//  CountriesData.h
+//  CountriesData
+//
+//  Created by JHONATAHAN RIVERA on 11/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CountriesData.
+FOUNDATION_EXPORT double CountriesDataVersionNumber;
+
+//! Project version string for CountriesData.
+FOUNDATION_EXPORT const unsigned char CountriesDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountriesData/PublicHeader.h>
+
+
